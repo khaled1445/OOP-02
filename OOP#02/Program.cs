3c6818d294066a1be67105dafd5e8258452ec06c
5aled998 <khaledamr142005@gmail.com>
2026-09-04 10:50:13 +0300

    base

--- OOP#02/Program.cs
+++ OOP#02/Program.cs
@@ -6,34 +6,32 @@ namespace OOP_02
     {
         static void Main(string[] args)
         {
-            //OOP 01 - Smart Delivery Management System 
+            //OOP 02 - Smart Delivery Management System 
             #region Part 01 : Theoretical Questions
 
-            #region Question 1 
-            //a- DeliveryAddress variable:
-            //first DeliveryAddress is a struct (value type) so when you take a copy in another value it makes
-            //a new seprate stackframe in the stack put in the new data and the old variable doesn't affected
+            #region Question 1
+            //Answer the following questions:
+            //a) What is the difference between a class and a struct?
 
-            //b- Customer variable:
-            // Customer is a class (Refrence Type) so when you take a copy you just take the refrence in the stack and
-            // it leads you to the same object in Heap so you'll OverWrite the old variable so the two variables will have
-            // the same refreance so tthe old one will change to whe new var
+            //b) Why are classes more suitable than structs for large applications?
 
             #endregion
 
-            #region Question02
-            //a) 1- all fields are public so it's not prevented and anyone can modify it
-            //2- there is no validation so the value can be negative 
-            //3- the input in description var can bee null 
+            #region Question 2 Consider the following code:
+            //a) Which class is the parent class?
 
-            //b) if you used private keywork instead of public now the code outside the struct canno't be acssessed outside the 
-            // struct so you can put the setters and getters to validate all fields  
+            //b) Which class is the child class?
+
+            //c) What members are inherited by ExpressShipment?
+
+            //d) Why is inheritance better than duplicating the same code in multiple classes?
+            //
             #endregion
 
             #endregion
 
 
-            #region Part 02 : Practical (Smart Delivery Management System)
+        #region Part 02 : Practical (Smart Delivery Management System)
 
 
 
